2d1864711f2d94c1ffe14f6e762ba16135cd2ca0
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2024-04-03 10:24:01 +0700

    create active link component

--- resources/views/components/navbar/dropdown-item.blade.php
+++ resources/views/components/navbar/dropdown-item.blade.php
@@ -1,3 +1,4 @@
-<a {{ $attributes }} class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">
+<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="page"-->
+<a {{ $attributes }} class="{{ request()->fullUrlIs(url($href)) ? "bg-zinc-700 text-white" : "text-zinc-300 hover:bg-zinc-800 hover:text-white" }} block rounded-md px-3 py-2 text-sm font-medium">
     {{ $slot }}
 </a>
