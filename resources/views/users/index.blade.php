922bef10afb1c7be5e36c25b4bd260b8bc6253d3
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2024-04-27 13:27:39 +0700

    add condition in component

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,23 +1,39 @@
 <x-app-layout title="Users">
     <x-slot name="heading">Users</x-slot>
-    <table>
-        <thead>
-        <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Created At</th>
-        </tr>
-        <tbody>
-        @foreach($users as $user)
-            <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $user->name }}</td>
-                <td>{{ $user->email }}</td>
-                <td>{{ $user->created_at->diffForHumans() }}</td>
-            </tr>
-        @endforeach
-        </tbody>
-        </thead>
-    </table>
+    <div class="sm:flex sm:items-center">
+        <x-section-title>
+            <x-slot name="title">Users</x-slot>
+            <x-slot name="description">
+                A list of all the users in your account including their name, title, email and role.
+            </x-slot>
+        </x-section-title>
+        <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+            <x-button as="a" href="/users/create">
+                Add User
+            </x-button>
+        </div>
+    </div>
+    <div class="mt-8 flow-root">
+        <x-table>
+            <x-table.thead>
+                <tr>
+                    <x-table.th>#</x-table.th>
+                    <x-table.th>Name</x-table.th>
+                    <x-table.th>Email</x-table.th>
+                    <x-table.th>Created At</x-table.th>
+                </tr>
+            <x-table.tbody>
+                @foreach($users as $user)
+                    <tr>
+                        <x-table.td>{{ $loop->iteration }}</x-table.td>
+                        <x-table.td>{{ $user->name }}</x-table.td>
+                        <x-table.td>{{ $user->email }}</x-table.td>
+                        <x-table.td>{{ $user->created_at->diffForHumans() }}</x-table.td>
+                    </tr>
+                @endforeach
+            </x-table.tbody>
+            </x-table.thead>
+        </x-table>
+    </div>
+
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,4 @@ Route::get('/contact', [Controllers\ContactController::class, 'index']);
 Route::get('/gallery', [Controllers\GalleryController::class, 'index']);
 Route::get('/users', [Controllers\UserController::class, 'index']);
 
-//Route::get('users', function() {
-//    $users = [
-//        ['id' => 1, 'name' => 'Rifqi Muhammad', 'email' => 'testing@gmail.com'],
-//        ['id' => 2, 'name' => 'Andy', 'email' => 'testing@gmail.com'],
-//    ];
-//
-//    return view('users.index', compact('users'));
-//});
+
